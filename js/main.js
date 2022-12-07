02bfbdac8813e28f859881fc5aa08a8f5518af08
José Blanco <jblanper@gmail.com>
2022-12-07 21:53:26 +0100

    new page

--- js/main.js
+++
@@ -1,9 +0,0 @@
-const header = document.querySelector('header');
-
-window.addEventListener('scroll', e => {
-    if (document.scrollingElement.scrollTop >= 20) {
-        header.classList.add('scrolled');
-    } else if (document.scrollingElement.scrollTop < 20) {
-        header.classList.remove('scrolled');
-    }
-});
